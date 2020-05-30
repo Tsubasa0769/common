a5f9041375d7811fdce06c8620321ef11e8c9522
chenzhicheng <284212657@qq.com>
2020-05-30 11:55:34 +0800

    比例分配编写
    Signed-off-by: chenzhicheng <284212657@qq.com>

---
+++ 比例分配编写.php
@@ -0,0 +1,22 @@
+<?php
+$a = ['id' => 1, 'weight' => 5, 'num' => 0];
+$b = ['id' => 2, 'weight' => 7, 'num' => 0];
+$c = ['id' => 3, 'weight' => 8, 'num' => 0];
+$arr = [$a,$b,$c];
+$sum = 0;
+$sumweight = 20;
+foreach($arr as $k => $v){
+	$sum += $v['num'];
+	$arr[$k]['pre'] = floor($v['weight'] / $sumweight * 100);
+}
+for($i=0;$i<100;$i++){
+	$sum++;
+	$compareArr = [];
+	foreach($arr as $k => $v) {
+		$compareArr[$k] = $sum * $v['weight'] - $v['num'] * $sumweight;
+	}
+	$index = array_search(max($compareArr),$compareArr);
+	echo $index.'</br>';
+	$arr[$index]['num']++;
+}
+print_r($arr);

ba6ad0aea47f20542443fbddf9f9084e287dc0ef
chenzhicheng <284212657@qq.com>
2021-04-20 10:44:01 +0800

    up
    Signed-off-by: chenzhicheng <284212657@qq.com>

--- 比例分配编写.php
+++ 比例分配编写.php
@@ -20,3 +20,80 @@ for($i=0;$i<100;$i++){
 	$arr[$index]['num']++;
 }
 print_r($arr);
+
+
+
+
+
+<?php
+//方法一
+$a = ['id' => 1, 'weight' => 5, 'num' => 0];
+$b = ['id' => 2, 'weight' => 5, 'num' => 0];
+$c = ['id' => 3, 'weight' => 5, 'num' => 0];
+$arr = [$a,$b,$c];
+$sumweight = 0;
+foreach($arr as $v){
+	$sumweight += $v['weight'];
+}
+foreach($arr as $k => $v){
+	$arr[$k]['pre'] = floor($v['weight'] / $sumweight * 100);
+}
+for($i=0;$i<100;$i++){
+	$num = mt_rand(0, $sumweight);
+	$start = 0;
+	foreach($arr as &$v){
+		$end = $start + $v['weight'];
+		if($start <= $num && $num < $end){
+			$v['num']++;
+			break;
+		}
+		$start = $end;
+	}
+}
+print_r($arr);
+
+
+
+
+//方法二
+$a = ['id' => 1, 'weight' => 5, 'num' => 20];
+$b = ['id' => 2, 'weight' => 5, 'num' => 14];
+$c = ['id' => 3, 'weight' => 5, 'num' => 20];
+$d = ['id' => 4, 'weight' => 1, 'num' => 0];
+$arr = [$a,$b,$c,$d];
+//客服总招的人数
+$sumnum = 0;
+//总比例
+$sumweight = 0;
+foreach($arr as $v){
+	$sumnum += $v['num'];
+	$sumweight += $v['weight'];
+}
+foreach($arr as $k => $v){
+	$arr[$k]['pre'] = floor($v['weight'] / $sumweight * 100);
+}
+for($i=0;$i<46;$i++){
+	$endnum = 0;
+	if(ceil($sumnum / $sumweight) == 0){
+		$loop = 1;
+	}elseif(ceil($sumnum / $sumweight) != 0 && $sumnum % $sumweight == 0){
+		$loop = ceil($sumnum / $sumweight) + 1;
+	}else{
+		$loop = ceil($sumnum / $sumweight);
+	}
+	foreach($arr as $v){
+		$endnum += $v['weight'] * $loop - $v['num'];
+	}
+	$num = mt_rand(0, $endnum-1);
+	$start = 0;
+	foreach($arr as $k => $v){
+		$end = $start + $v['weight'] * $loop - $v['num'];
+		if($start <= $num && $num < $end){
+			$arr[$k]['num']++;
+			break;
+		}
+		$start = $end;
+	}
+	$sumnum++;
+}
+print_r($arr);
